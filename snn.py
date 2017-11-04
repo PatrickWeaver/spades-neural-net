a1daa1200f6f501a55b76bc2493a1d1bd7db7d77
Patrick Weaver <pjpwea@gmail.com>
2017-11-04 18:08:04 -0400

    set up train and query methods

--- snn.py
+++ snn.py
@@ -1,12 +1,59 @@
-def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
-    # set number of nodes in each input, hidden, output layer
-    self.input_nodes = input_nodes
-    self.hidden_nodes = hidden_nodes
-    self.output_nodes = output_nodes
+import numpy
+import scipy.special
 
-    # learning rate
-    self.learning_rate = learning_rate
-    pass
+class neuralNetwork
+
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+        # set number of nodes in each input, hidden, output layer
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+
+
+
+        # Link weight matricies, weight_input_hidden and weight_hidden_output
+        # Weights inside the arrays are w_i_j, where link is form node i to node j in the next layer
+
+        self.weight_input_hidden = (numpy.random.rand(self.hidden_nodes, self.input_nodes) - 0.5)
+        self.weight_hidden_output = (numpy.random.rand(self.output_nodes, self.hidden_nodes) - 0.5)
+
+        # More sophisticated weight initialization:
+        #self.weight_input_hidden = numpy.random.normal (0.0, pow(self.hidden_nodes, -0.5), (self.hidden_nodes, self.input_nodes))
+        #self.weight_hidden_output = numpy.random.normal (0.0, pow(self.output_nodes, -0.5), (self.output_nodes, self.hidden_nodes))
+
+        # learning rate
+        self.learning_rate = learning_rate
+
+        # define activation_function, which is the sigmoid function
+        self.activation_function = lambda x: scipy.special.expit(x)
+
+        pass
+
+    def train():
+        pass
+
+    def query(self, inputs_list):
+        # Convert inputs list to 2D array:
+        inputs = numpy.array(inputs_list, ndmin=2).T
+
+
+        #
+        hidden_inputs = numpy.dot(self.weight_input_hidden, inputs)
+
+        # calculate the signals into hidden layer
+        hidden_inputs = numpy.dot(self.weight_input_hidden, inputs)
+        # calculate the signals emerging from the hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        #calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+        #calculate signals into final output layer
+        final_inputs = numpy.dot(self.weight_hidden_output, hidden_outputs)
+
+        #calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
 
 
 input_nodes = 3
